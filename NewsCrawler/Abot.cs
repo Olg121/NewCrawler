a20c5d2bd01e1bfeef0a79d82c3a1c8fe7117dba
Raman Svezhantsau <raman.svezhantsau@gmail.com>
2019-03-31 17:58:49 +0300

    Проверка на наличте бд и создание в случае отсутствия

--- NewsCrawler/Abot.cs
+++ NewsCrawler/Abot.cs
@@ -21,11 +21,10 @@ namespace NewsCrawler
             Uri uriToCrawl = GetSiteToCrawl(URL);
 
             IWebCrawler crawler;
-
-            //Uncomment only one of the following to see that instance in action
+      
             //crawler = GetDefaultWebCrawler();
-            crawler = GetManuallyConfiguredWebCrawler();
-            //crawler = GetCustomBehaviorUsingLambdaWebCrawler();
+            //crawler = GetManuallyConfiguredWebCrawler();
+            crawler = GetCustomBehaviorUsingLambdaWebCrawler();
 
             //Subscribe to any of these asynchronous events, there are also sychronous versions of each.
             //This is where you process data about specific events of the crawl
@@ -35,12 +34,8 @@ namespace NewsCrawler
             crawler.PageLinksCrawlDisallowedAsync += crawler_PageLinksCrawlDisallowed;
 
             //Start the crawl
-            //This is a synchronous call
             CrawlResult result = crawler.Crawl(uriToCrawl);
 
-            //Now go view the log.txt file that is in the same directory as this executable. It has
-            //all the statements that you were trying to read in the console window :).
-            //Not enough data being logged? Change the app.config file's log4net log level from "INFO" TO "DEBUG"
             
             return "ok"; 
         }
@@ -79,31 +74,14 @@ namespace NewsCrawler
         {
             IWebCrawler crawler = GetDefaultWebCrawler();
 
-            //Register a lambda expression that will make Abot not crawl any url that has the word "ghost" in it.
-            //For example http://a.com/ghost, would not get crawled if the link were found during the crawl.
-            //If you set the log4net log level to "DEBUG" you will see a log message when any page is not allowed to be crawled.
-            //NOTE: This is lambda is run after the regular ICrawlDecsionMaker.ShouldCrawlPage method is run.
             crawler.ShouldCrawlPage((pageToCrawl, crawlContext) =>
             {
-                if (pageToCrawl.Uri.AbsoluteUri.Contains("ghost"))
-                    return new CrawlDecision { Allow = false, Reason = "Scared of ghosts" };
+                if (pageToCrawl.Uri.AbsoluteUri.Contains("news"))
+                    return new CrawlDecision { Allow = true };
 
-                return new CrawlDecision { Allow = true };
+                return new CrawlDecision { Allow = false };
             });
 
-            //Register a lambda expression that will tell Abot to not download the page content for any page after 5th.
-            //Abot will still make the http request but will not read the raw content from the stream
-            //NOTE: This lambda is run after the regular ICrawlDecsionMaker.ShouldDownloadPageContent method is run
-            crawler.ShouldDownloadPageContent((crawledPage, crawlContext) =>
-            {
-                if (crawlContext.CrawledCount >= 5)
-                    return new CrawlDecision { Allow = false, Reason = "We already downloaded the raw page content for 5 pages" };
-
-                return new CrawlDecision { Allow = true };
-            });
-
-            //Register a lambda expression that will tell Abot to not crawl links on any page that is not internal to the root uri.
-            //NOTE: This lambda is run after the regular ICrawlDecsionMaker.ShouldCrawlPageLinks method is run
             crawler.ShouldCrawlPageLinks((crawledPage, crawlContext) =>
             {
                 if (!crawledPage.IsInternal)
--- NewsCrawler/Program.cs
+++ NewsCrawler/Program.cs
@@ -29,15 +29,33 @@ namespace NewsCrawler
         }
         static void DBCreate()
         {
-            NpgsqlConnection npgSqlConnection = new NpgsqlConnection("Server=localhost;Port=5432;Username=postgres;Password=1122;Database=");
+
+            // нужно изменять пароль в строке коннекта, можно добавить в интерфейс
+            NpgsqlConnection npgSqlConnection = new NpgsqlConnection("Server=localhost;Port=5432;Username=postgres;Password=qwerty;Database=news");
+            //  NpgsqlConnection npgSqlConnection = new NpgsqlConnection("Server=localhost;Port=5432;Username={0};Password={1};Database=news", login, passw);
             try
             {
                 npgSqlConnection.Open();
             }
             catch
             {
-               // Создание базы данных 
-               // Добавление таблицы 
+                // создает дб с названием news, если ее нет
+                npgSqlConnection = new NpgsqlConnection("Server=localhost;Port=5432;Username=postgres;Password=qwerty");
+                // npgSqlConnection = new NpgsqlConnection("Server=localhost;Port=5432;Username={0};Password={1};", login, passw);
+                NpgsqlCommand createDb = new NpgsqlCommand("create database news;", npgSqlConnection);
+                npgSqlConnection.Open();       
+                createDb.ExecuteNonQuery();
+                npgSqlConnection.Close();
+
+                // подключаемся к только что созданной бд news и создаем в ней таблицу data
+                npgSqlConnection = new NpgsqlConnection("Server=localhost;Port=5432;Username=postgres;Password=qwerty;Database=news");
+                // npgSqlConnection = new NpgsqlConnection("Server=localhost;Port=5432;Username={0};Password={1};Database=news", login, passw);
+                NpgsqlCommand createTbl = new NpgsqlCommand("create table data (url varchar, title varchar, content text, html text);", npgSqlConnection);
+                npgSqlConnection.Open();
+                createTbl.ExecuteNonQuery();
+                npgSqlConnection.Close();
+
+               
             }
             finally
             {
